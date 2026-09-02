60c8ff11d30bae8c9fb68c899200a1da92d4d27f
PG-23 <patrick_guinn@yahoo.com>
2026-09-02 15:27:16 -0500

    Add additional Week 2 challenges

--- RevatureAssignments/Week2/RevatureChallenges.java
+++ RevatureAssignments/Week2/RevatureChallenges.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RevatureChallenges {
@@ -122,5 +124,95 @@ public class RevatureChallenges {
             again = "n";
         }
         System.out.println("Thank you for using the calculator.");
+
+        // Week 2 Challenge - Calculate 5 Test Scores
+        // Please note this challenge does not have any error handling
+        // and test scores must be input as space separated values on
+        // the same line
+        System.out.println("\nCalculate 5 Test Scores Challenge"
+                + "\nPlease enter 5 test scores: ");
+
+        Scanner kbd = new Scanner(System.in);
+        int numScores = 5;
+        Integer[] scores = new Integer[numScores];
+
+        for (i = 0; i < numScores; i++) {
+            scores[i] = kbd.nextInt();
+        }
+        kbd.nextLine();
+        int totalScore = 0;
+        int maxScore = scores[0];
+        int minScore = scores[0];
+
+        for (Integer s: scores) {
+            totalScore += s;
+            if (s > maxScore) {
+                maxScore = s;
+            }
+            if (s < minScore) {
+                minScore = s;
+            }
+        }
+
+        int avgScore = totalScore / numScores;
+        System.out.printf("Total: %d%nAverage: %d%nHighest: %d%nLowest: %d%n",
+                totalScore, avgScore, maxScore, minScore);
+
+        System.out.println("Your values were:");
+        for (Integer s: scores) {
+            if (s >= 90) {
+                grade = 'A';
+            } else if (s >= 80) {
+                grade = 'B';
+            } else if (s >= 70) {
+                grade = 'C';
+            } else if (s >= 60){
+                grade = 'D';
+            } else {
+                grade = 'F';
+            }
+            System.out.println(s + " - " + grade);
+        }
+
+        // Week 2 Challenge - REPL
+        System.out.println("\nREPL Challenge");
+        double balance = 0.0;
+        int input;
+        kbd = new Scanner(System.in);
+        do {
+            System.out.println("\nPlease select an option below:");
+            System.out.println( "1. Check Balance\n" +
+                                "2. Deposit\n" +
+                                "3. Withdraw\n" +
+                                "4. Exit\n");
+
+            input = Integer.parseInt(kbd.nextLine());
+            double amt;
+            switch (input) {
+                case 1:
+                    System.out.printf("Current Balance: $%.2f%n", balance);
+                    break;
+                case 2:
+                    System.out.println("Please enter a deposit amount:");
+                    amt = Double.parseDouble(kbd.nextLine());
+                    balance += amt;
+                    System.out.printf("Transaction successful - You have deposited $%.2f%n", amt);
+                    break;
+                case 3:
+                    System.out.println("Please enter a withdrawal amount:");
+                    amt = Double.parseDouble(kbd.nextLine());
+                    if (balance - amt < 0) {
+                        System.out.println("Transaction failed - Insufficient funds");
+                    } else {
+                        balance -= amt;
+                        System.out.printf("Transaction successful - You have withdraw $%.2f%n", amt);
+                    }
+                    break;
+                case 4:
+                    System.out.println("You are now exiting the menu");
+                    break;
+            }
+        } while (input != 4);
+        kbd.close();
     }
 }
